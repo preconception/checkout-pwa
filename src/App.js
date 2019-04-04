171d26138d16378873774f6dc9459d8f75e53044
Glitch (create-react-app-sample) <none>
2019-04-04 03:42:30 +0000

    ⏱🌕 Checkpoint ./package.json:3729278/21 ./public/index.html:3729278/54 ./src/App.js:3729278/1400

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      stripe: window.Stripe('pk_test_Yveko5sGm189roOBqeSk6lgb'), 
+      error: null
+    };
+  }
+  
+  onPayButtonClick(e) {
+    this.state.stripe.redirectToCheckout({
+      items: [
+        // Replace with the ID of your SKU
+        {sku: 'sku_123', quantity: 1}
+      ],
+      successUrl: 'https://your-website.com/success',
+      cancelUrl: 'https://your-website.com/canceled',
+    }).then(function (result) {
+      // Display result.error.message to your customer
+      this.setState({
+        error: result.error.message
+      });
+    });
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +38,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <button className="pay-button" onClick={this.onPayButtonClick}>Pay</button>
+        </div>
+        {this.state.error ? (
+          <div className="error">
+            {this.state.error}
+          </div>  
+        ) : null}        
       </div>
     );
   }
